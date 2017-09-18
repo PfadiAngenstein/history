cf2b4c017b51597457e140498a370b86e6aeda40
grujo <faitnoise@users.noreply.github.com>
2017-09-18 10:48:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -281,6 +281,7 @@
     <footer>
         <p class="float-left center-on-mobile">&copy; 2016 - <?php echo date("Y"); ?> Pfadi Angenstein</p>
         <p class="float-right center-on-mobile"><a href="https://www.pfadiangenstein.ch">www.pfadiangenstein.ch</a></p>
+        <div class="clear">
     </footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
